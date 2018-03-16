48dc54680c322630281d4a7ddbc74daa6289b133
esteban <efmino@uc.cl>
2018-03-16 13:17:27 -0700

    base components

---
+++ betterjourneyui/src/components/messenger/Chat.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+  render() {
+    return (
+      <div className="Chat">
+      </div>
+    );
+  }
+}
+
+export default Chat;
---
+++ betterjourneyui/src/components/messenger/ChatsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ChatsList extends Component {
+  render() {
+    return (
+      <div className="ChatsList">
+      </div>
+    );
+  }
+}
+
+export default ChatsList;
